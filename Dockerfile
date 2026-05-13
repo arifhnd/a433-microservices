9c3e2de908795b046bdda09f68091d9597dfbe7b
arifhnd <forevertkj4@gmail.com>
2026-05-13 21:08:23 +0700

    Submission ready

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
+# Menggunakan base image Node.js versi 14
 FROM node:14
 
+# Membuat direktori kerja di dalam container
 WORKDIR /app
 
+# Menyalin seluruh project ke dalam direktori kerja di container
 COPY . .
 
+# Menentukan environment variable untuk aplikasi
 ENV NODE_ENV=production DB_HOST=item-db
 
+# Menginstal dependencies dan melakukan build aplikasi
 RUN npm install --production --unsafe-perm && npm run build
 
+# Menjalankan aplikasi dengan perintah npm start 
 CMD ["npm", "start"]
 
+# Mengekspos port 8080 untuk aplikasi
 EXPOSE 8080
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,5 +1,14 @@
+# Melakukan build image dengan nama item-app dan tag v1
 docker build -t item-app:v1 .
+
+# Menampilkan daftar image yang telah dibuat
 docker images
-docker tag item-app:v1 fazerdaze/item-app:v1
-echo $PASSWORD_DOCKER_HUB | docker login -u fazerdaze --password-stdin
-docker push fazerdaze/item-app:v1
+
+# Menandai image item-app:v1 dengan nama repository arifhnd/item-app dan tag v1
+docker tag item-app:v1 ghcr.io/arifhnd/item-app:v1
+
+# Melakukan login ke GitHub Container Registry menggunakan token yang disimpan di environment variable CR_PAT
+echo $CR_PAT | docker login ghcr.io -u arifhnd --password-stdin
+
+# Melakukan push image ke GitHub Container Registry
+docker push ghcr.io/arifhnd/item-app:v1
